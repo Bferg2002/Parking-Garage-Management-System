cfcbe430ba370ac73d50ab28605cceb71090b008
michaelmoss <mxmoss261@gmail.com>
2026-03-19 16:53:00 -0400

    michaelmoss 26q1-team4-addedtestfile

--- src/test/java/AppUserConstruction/EmployeeTest.java
+++ src/test/java/AppUserConstruction/EmployeeTest.java
@@ -1,12 +1,67 @@
 package AppUserConstruction;
+
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class EmployeeTest {
+import VehicleSorting.Vehicle;
+
+class EmployeeTest {
+
     @Test
-    public void testEmployeeName() {
+    void testConstructorAndGetters() {
+        Employee emp = new Employee(101, "Michael");
 
+        assertEquals("Michael", emp.getName());
+        assertEquals(101, emp.getEmployeeId());
     }
 
-}//ends class
+    @Test
+    void testSetters() {
+        Employee emp = new Employee(101, "Michael");
 
+        emp.setEmployeeId(202);
+        emp.setRole("Manager");
+
+        assertEquals(202, emp.getEmployeeId());
+        assertEquals("Manager", emp.getRole());
+    }
+
+    @Test
+    void testCalculateExpense() {
+        Employee emp = new Employee(101, "Michael");
+
+        double expense = emp.calculateExpense(null);
+
+        assertEquals(100.0, expense);
+    }
+
+    @Test
+    void testCheckRegistration() {
+        Employee emp = new Employee(101, "Michael");
+
+        boolean result = emp.checkRegistration(null);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testTrackEntryAndExit() {
+        Employee emp = new Employee(101, "Michael");
+
+        emp.trackEntry(null);
+        emp.trackExit(null);
+
+        // No assertion needed (just coverage)
+    }
+
+    @Test
+    void testDisplayRole() {
+        Employee emp = new Employee(101, "Michael");
+        emp.setRole("Developer");
+
+        emp.displayRole();
+
+        // Covers abstract method implementation
+    }
+}
